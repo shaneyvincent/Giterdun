ccd7c9794e33bc79c0440930e6c0f04d792263ca
Shane Vincent <shaneyshaneshane5@gmail.com>
2016-04-13 18:52:18 -0400

    whattimeisit

---
+++ README.md
@@ -0,0 +1,16 @@
+
+"dis da read me"
+ "Thanks for choosing ,
+SHANEYS HOMEWORK HELP n ANSWERS fool!
+like n share,
+to locate the problem for this file go to.."
+
+
+"Week 3: Intro To The Web"
+
+"Ruby Gems"
+
+""WHAT TIME IS IT?"
+
+"How do you get the current time in Ruby?
+ Does this require ActiveSupport?"
---
+++ whattimeisit.rb
@@ -0,0 +1,13 @@
+puts "SHANEYS HOMEWORK HELP n ANSWERS"
+
+puts "##################################"
+
+puts "How do you get the current time in Ruby? Does this require ActiveSupport?"
+
+puts "##################################"
+
+puts "first launch active support which is required to run the time
+irb -r 'active_support/all'
+then run
+Time.now
+presto!"
